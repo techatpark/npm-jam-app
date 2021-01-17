2cc996077187a4b843a1ff4e621c0f76006d0690
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-01-17 23:25:25 +0530

    login

--- browser-sync.js
+++ browser-sync.js
@@ -19,7 +19,28 @@ module.exports = {
      | https://github.com/shakyShane/browser-sync/wiki/options#wiki-files
      */
     files: ["dist/css/*.css", "dist/js/*.js","dist/**/*.html"],
-    proxy: "localhost:800",
+    middleware: [
+        {
+            route: "/api",
+            handle: function (req, res, next) {
+
+                res.setHeader('Content-Type', 'application/json');
+
+                if(req.url == "/login") {
+                    if(req.method == 'POST') {
+                        const username = req.body.username;
+                        const password = req.body.password;
+                        res.statusCode = 200;
+                        res.end('Hello, World!\n');
+                    }else {
+                        res.statusCode = 405;
+                        res.end('Invalid\n');
+                    }
+                }
+                next();
+            }
+        }
+    ],
     serveStatic: ['dist']
 
 };
