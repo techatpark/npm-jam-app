704b40721cfc70617c1b5e0c4c0976c9fb29c91d
sathishk <sathishkumar.thiyagarajan@gmail.com>
2021-01-18 18:26:58 +0530

    Taje latest

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,19 @@
-function login() {
-    // Veruify Login
-    window.location.href = '/'; 
+function login(event) {
+
+    // Where did the event happend
+    const form = event.currentTarget;
+
+    // Collect Data from Form
+    const username = form.elements["inputEmail"].value; 
+    const password = form.elements["inputPassword"].value; 
+
+    // Perform Logic
+    if(username == "2@3.om" && password == "password") {
+        window.location.href = '/'; 
+    }
+    else {
+        alert("Invalid User")
+    }
+    
     return false;
 }
